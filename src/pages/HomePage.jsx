7a9e7fb47ae43e019abc8076a01281e2e97cfcc3
Christian <chritiangabriel4@gmail.com>
2023-12-11 20:13:29 -0300

    add pages end modifications

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import './Headers.css';
+
+const HomePage = () => {
+    return (
+        <div>
+            <header>
+                <div className='headerHome'>
+                    <h1 className="headerHomeText">Minha Pagina Pessoal</h1>
+                </div>
+            </header>
+            <body>
+                
+            </body>
+        </div>
+    );
+}
+
+export default HomePage;
